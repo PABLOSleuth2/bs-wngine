c2edaa734ee120168d96d5a757e0d2d9c73e5699
PABLOSleuth2 <87932858+PABLOSleuth2@users.noreply.github.com>
2021-11-30 00:24:55 +0600

    Add files via upload

---
+++ source/ClientPrefs.hx
@@ -0,0 +1,205 @@
+package;
+
+import flixel.FlxG;
+import flixel.util.FlxSave;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+
+class ClientPrefs {
+	public static var downScroll:Bool = false;
+	public static var middleScroll:Bool = false;
+	public static var showFPS:Bool = true;
+	public static var flashing:Bool = true;
+	public static var globalAntialiasing:Bool = true;
+	public static var noteSplashes:Bool = false;
+	public static var lowQuality:Bool = false;
+	public static var framerate:Int = 60;
+	public static var cursing:Bool = true;
+	public static var violence:Bool = false;
+	public static var camZooms:Bool = true;
+	public static var hideHud:Bool = false;
+	public static var noteOffset:Int = 0;
+	public static var bruhshit:Float = 0.5;
+	public static var arrowHSV:Array<Array<Int>> = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]];
+	public static var imagesPersist:Bool = false;
+	public static var ghostTapping:Bool = true;
+	public static var hideTime:Bool = false;
+	public static var easteregg:Bool = false;
+	public static var sussybest:Bool = false;
+	public static var cutscene:Bool = false;
+	public static var girlfriend:Bool = false;
+	public static var idkwhy:Bool = false;
+
+	//Every key has two binds, add your key bind down here and then add your control on options/ControlsSubState.hx and Controls.hx
+	public static var keyBinds:Map<String, Array<FlxKey>> = [
+		//Key Bind, Name for ControlsSubState
+		'note_left'		=> [A, LEFT],
+		'note_down'		=> [S, DOWN],
+		'note_up'		=> [W, UP],
+		'note_right'	=> [D, RIGHT],
+		
+		'ui_left'		=> [A, LEFT],
+		'ui_down'		=> [S, DOWN],
+		'ui_up'			=> [W, UP],
+		'ui_right'		=> [D, RIGHT],
+		
+		'accept'		=> [SPACE, ENTER],
+		'back'			=> [BACKSPACE, ESCAPE],
+		'pause'			=> [ENTER, ESCAPE],
+		'reset'			=> [R, NONE],
+	];
+	public static var defaultKeys:Map<String, Array<FlxKey>> = null;
+
+		public static function loadDefaultKeys() {
+		defaultKeys = keyBinds.copy();
+		//trace(defaultKeys);
+	}
+
+	public static function saveSettings() {
+		FlxG.save.data.downScroll = downScroll;
+		FlxG.save.data.middleScroll = middleScroll;
+		FlxG.save.data.showFPS = showFPS;
+		FlxG.save.data.flashing = flashing;
+		FlxG.save.data.globalAntialiasing = globalAntialiasing;
+		FlxG.save.data.noteSplashes = noteSplashes;
+		FlxG.save.data.lowQuality = lowQuality;
+		FlxG.save.data.framerate = framerate;
+		FlxG.save.data.violence = violence;
+		FlxG.save.data.camZooms = camZooms;
+		FlxG.save.data.noteOffset = noteOffset; 
+		FlxG.save.data.bruhshit = bruhshit;
+		FlxG.save.data.hideHud = hideHud;
+		FlxG.save.data.arrowHSV = arrowHSV;
+		FlxG.save.data.imagesPersist = imagesPersist;
+		FlxG.save.data.ghostTapping = ghostTapping;
+		FlxG.save.data.hideTime = hideTime;
+		FlxG.save.data.achievementsMap = Achievements.achievementsMap;
+		FlxG.save.data.henchmenDeath = Achievements.henchmenDeath;
+		FlxG.save.data.cutscene = cutscene;
+		FlxG.save.data.girlfriend = girlfriend;
+		FlxG.save.data.sussybest = sussybest;
+		FlxG.save.data.easteregg = easteregg;
+		FlxG.save.data.idkwhy = idkwhy;
+		FlxG.save.flush();
+
+		var save:FlxSave = new FlxSave();
+		save.bind('controls_v2', 'ninjamuffin99'); //Placing this in a separate save so that it can be manually deleted without removing your Score and stuff
+		save.data.customControls = keyBinds;
+		save.flush();
+		FlxG.log.add("Settings saved!");
+	}
+
+	public static function loadPrefs() {
+		if(FlxG.save.data.downScroll != null) {
+			downScroll = FlxG.save.data.downScroll;
+		}
+		if(FlxG.save.data.middleScroll != null) {
+			middleScroll = FlxG.save.data.middleScroll;
+		}
+		if(FlxG.save.data.showFPS != null) {
+			showFPS = FlxG.save.data.showFPS;
+			if(Main.fpsVar != null) {
+				Main.fpsVar.visible = showFPS;
+			}
+		}
+		if(FlxG.save.data.idkwhy != null) {
+			idkwhy = FlxG.save.data.idkwhy;
+		}
+		if(FlxG.save.data.sussybest != null) {
+			sussybest = FlxG.save.data.sussybest;
+		}
+		if(FlxG.save.data.cutscene != null) {
+			cutscene = FlxG.save.data.cutscene;
+		}
+		if(FlxG.save.data.girlfriend != null) {
+			girlfriend = FlxG.save.data.girlfriend;
+		}
+		if(FlxG.save.data.flashing != null) {
+			flashing = FlxG.save.data.flashing;
+		}
+		if(FlxG.save.data.easteregg != null) {
+			easteregg = FlxG.save.data.easteregg;
+		}
+		if(FlxG.save.data.globalAntialiasing != null) {
+			globalAntialiasing = FlxG.save.data.globalAntialiasing;
+		}
+		if(FlxG.save.data.noteSplashes != null) {
+			noteSplashes = FlxG.save.data.noteSplashes;
+		}
+		if(FlxG.save.data.lowQuality != null) {
+			lowQuality = FlxG.save.data.lowQuality;
+		}
+		if(FlxG.save.data.framerate != null) {
+			framerate = FlxG.save.data.framerate;
+			if(framerate > FlxG.drawFramerate) {
+				FlxG.updateFramerate = framerate;
+				FlxG.drawFramerate = framerate;
+			} else {
+				FlxG.drawFramerate = framerate;
+				FlxG.updateFramerate = framerate;
+			}
+		}
+		/*if(FlxG.save.data.cursing != null) {
+			cursing = FlxG.save.data.cursing;
+		}*/
+		if(FlxG.save.data.violence != null) {
+			violence = FlxG.save.data.violence;
+		}
+		if(FlxG.save.data.camZooms != null) {
+			camZooms = FlxG.save.data.camZooms;
+		}
+		if(FlxG.save.data.hideHud != null) {
+			hideHud = FlxG.save.data.hideHud;
+		}
+		if(FlxG.save.data.noteOffset != null) {
+			noteOffset = FlxG.save.data.noteOffset;
+		} 
+		if(FlxG.save.data.bruhshit != null) {
+			bruhshit = FlxG.save.data.bruhshit;
+		}
+		if(FlxG.save.data.arrowHSV != null) {
+			arrowHSV = FlxG.save.data.arrowHSV;
+		}
+		if(FlxG.save.data.imagesPersist != null) {
+			imagesPersist = FlxG.save.data.imagesPersist;
+			FlxGraphic.defaultPersist = ClientPrefs.imagesPersist;
+		}
+		if(FlxG.save.data.ghostTapping != null) {
+			ghostTapping = FlxG.save.data.ghostTapping;
+		}
+		if(FlxG.save.data.hideTime != null) {
+			hideTime = FlxG.save.data.hideTime;
+		}
+
+		var save:FlxSave = new FlxSave();
+		save.bind('controls_v2', 'ninjamuffin99');
+		if(save != null && save.data.customControls != null) {
+			var loadedControls:Map<String, Array<FlxKey>> = save.data.customControls;
+			for (control => keys in loadedControls) {
+				keyBinds.set(control, keys);
+			}
+			reloadControls();
+		}
+	}
+
+	public static function reloadControls() {
+		PlayerSettings.player1.controls.setKeyboardScheme(KeyboardScheme.Solo);
+	}
+
+	public static function copyKey(arrayToCopy:Array<FlxKey>):Array<FlxKey> {
+		var copiedArray:Array<FlxKey> = arrayToCopy.copy();
+		var i:Int = 0;
+		var len:Int = copiedArray.length;
+
+		while (i < len) {
+			if(copiedArray[i] == NONE) {
+				copiedArray.remove(NONE);
+				--i;
+			}
+			i++;
+			len = copiedArray.length;
+		}
+		return copiedArray;
+	}
+}
