334f786c3d380e41a1b28a92a842a9e7d91b083e
PABLOSleuth2 <87932858+PABLOSleuth2@users.noreply.github.com>
2021-11-30 11:19:02 +0600

    Create Achievements.hx

---
+++ source/Achievements.hx
@@ -0,0 +1,182 @@
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxCamera;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.group.FlxSpriteGroup;
+import flixel.util.FlxColor;
+import flixel.text.FlxText;
+
+using StringTools;
+
+class Achievements {
+	public static var achievementsStuff:Array<Dynamic> = [ //Name, Description, Achievement save tag, Hidden achievement
+		["Freaky on a Friday Night",	"Jogue numa Sexta... A noite... YEAH.",						'friday_night_play',	 true],
+		["Boa tentativa velhote",		"Vença a week 1 no Hard sem errar.",				'week1_nomiss',			false],
+		["Sem mais travessuras só doçuras",				"Vença a week 2 no Hard sem errar.",				'week2_nomiss',			false],
+		["Me chame de o matador",			"Vença a week 3 no Hard sem errar.",				'week3_nomiss',			false],
+		["Até em alta velocidade",					"Vença a week 4 no Hard sem errar.",				'week4_nomiss',			false],
+		["Um natal sem vacilos",			"Vença a week 5 no Hard sem errar.",				'week5_nomiss',			false],
+		["A mim que elas querem agora!!",					"Vença a week 6 no Hard sem errar.",				'week6_nomiss',			false],
+		["[[HIPERLINK BLOCKED]]",			"Ache a cutscene secreta dentro do game.",				'week7_nomiss',			 true],
+		["Isso foi um Funkin' Disastre!",	"Acerte menos que 20% da musica.",	'ur_bad',				false],
+		["Perfecionista",				"tenha 100% em uma musica, confio em ti.",			'ur_good',				false],
+		["Roadkill Enthusiast",			"Watch the Henchmen die over 100 times.",			'roadkill_enthusiast',	false],
+		["Cantando, mas tipo MUITO...?",		"Segure uma nota por 20 segundos",					'oversinging',			false],
+		["Hiperativo",					"Termine uma musica sem voltar para a animacao padrao.",				'hype',					false],
+		["Só nós dois",			"Termine uma musica com apenas duas teclas.",			'two_keys',				false],
+		["Torradeira Gamer",				"Por acaso, voce jogou o mod numa torradeira?",		'toastie',				false],
+		["Debugger",					"Ganhe o \"Test\" pelo Editor de Chart.",	'debugger',				 true]
+	];
+	public static var achievementsMap:Map<String, Bool> = new Map<String, Bool>();
+
+	public static var henchmenDeath:Int = 0;
+	public static function unlockAchievement(name:String):Void {
+		FlxG.log.add('Completed achievement "' + name +'"');
+		achievementsMap.set(name, true);
+		FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
+	}
+
+	public static function isAchievementUnlocked(name:String) {
+		if(achievementsMap.exists(name) && achievementsMap.get(name)) {
+			return true;
+		}
+		return false;
+	}
+
+	public static function getAchievementIndex(name:String) {
+		for (i in 0...achievementsStuff.length) {
+			if(achievementsStuff[i][2] == name) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static function loadAchievements():Void {
+		if(FlxG.save.data != null) {
+			if(FlxG.save.data.achievementsMap != null) {
+				achievementsMap = FlxG.save.data.achievementsMap;
+			}
+			if(FlxG.save.data.achievementsUnlocked != null) {
+				FlxG.log.add("Trying to load stuff");
+				var savedStuff:Array<String> = FlxG.save.data.achievementsUnlocked;
+				for (i in 0...savedStuff.length) {
+					achievementsMap.set(savedStuff[i], true);
+				}
+			}
+			if(henchmenDeath == 0 && FlxG.save.data.henchmenDeath != null) {
+				henchmenDeath = FlxG.save.data.henchmenDeath;
+			}
+		}
+
+		// You might be asking "Why didn't you just fucking load it directly dumbass??"
+		// Well, Mr. Smartass, consider that this class was made for Mind Games Mod's demo,
+		// i'm obviously going to change the "Psyche" achievement's objective so that you have to complete the entire week
+		// with no misses instead of just Psychic once the full release is out. So, for not having the rest of your achievements lost on
+		// the full release, we only save the achievements' tag names instead. This also makes me able to rename
+		// achievements later as long as the tag names aren't changed of course.
+
+		// Edit: Oh yeah, just thought that this also makes me able to change the achievements orders easier later if i want to.
+		// So yeah, if you didn't thought about that i'm smarter than you, i think
+
+		// buffoon
+	}
+}
+
+class AttachedAchievement extends FlxSprite {
+	public var sprTracker:FlxSprite;
+	private var tag:String;
+	public function new(x:Float = 0, y:Float = 0, name:String) {
+		super(x, y);
+
+		changeAchievement(name);
+		antialiasing = ClientPrefs.globalAntialiasing;
+	}
+
+	public function changeAchievement(tag:String) {
+		this.tag = tag;
+		reloadAchievementImage();
+	}
+
+	public function reloadAchievementImage() {
+		if(Achievements.isAchievementUnlocked(tag)) {
+			loadGraphic(Paths.image('achievementgrid'), true, 150, 150);
+			animation.add('icon', [Achievements.getAchievementIndex(tag)], 0, false, false);
+			animation.play('icon');
+		} else {
+			loadGraphic(Paths.image('lockedachievement'));
+		}
+		scale.set(0.7, 0.7);
+		updateHitbox();
+	}
+
+	override function update(elapsed:Float) {
+		if (sprTracker != null)
+			setPosition(sprTracker.x - 130, sprTracker.y + 25);
+
+		super.update(elapsed);
+	}
+}
+
+class AchievementObject extends FlxSpriteGroup {
+	public var onFinish:Void->Void = null;
+	var alphaTween:FlxTween;
+	public function new(name:String, ?camera:FlxCamera = null)
+	{
+		super(x, y);
+		ClientPrefs.saveSettings();
+
+		var id:Int = Achievements.getAchievementIndex(name);
+		var achievementBG:FlxSprite = new FlxSprite(60, 50).makeGraphic(420, 120, FlxColor.BLACK);
+		achievementBG.scrollFactor.set();
+
+		var achievementIcon:FlxSprite = new FlxSprite(achievementBG.x + 10, achievementBG.y + 10).loadGraphic(Paths.image('achievementgrid'), true, 150, 150);
+		achievementIcon.animation.add('icon', [id], 0, false, false);
+		achievementIcon.animation.play('icon');
+		achievementIcon.scrollFactor.set();
+		achievementIcon.setGraphicSize(Std.int(achievementIcon.width * (2 / 3)));
+		achievementIcon.updateHitbox();
+		achievementIcon.antialiasing = ClientPrefs.globalAntialiasing;
+
+		var achievementName:FlxText = new FlxText(achievementIcon.x + achievementIcon.width + 20, achievementIcon.y + 16, 280, Achievements.achievementsStuff[id][0], 16);
+		achievementName.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT);
+		achievementName.scrollFactor.set();
+
+		var achievementText:FlxText = new FlxText(achievementName.x, achievementName.y + 32, 280, Achievements.achievementsStuff[id][1], 16);
+		achievementText.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT);
+		achievementText.scrollFactor.set();
+
+		add(achievementBG);
+		add(achievementName);
+		add(achievementText);
+		add(achievementIcon);
+
+		var cam:Array<FlxCamera> = FlxCamera.defaultCameras;
+		if(camera != null) {
+			cam = [camera];
+		}
+		alpha = 0;
+		achievementBG.cameras = cam;
+		achievementName.cameras = cam;
+		achievementText.cameras = cam;
+		achievementIcon.cameras = cam;
+		alphaTween = FlxTween.tween(this, {alpha: 1}, 0.5, {onComplete: function (twn:FlxTween) {
+			alphaTween = FlxTween.tween(this, {alpha: 0}, 0.5, {
+				startDelay: 2.5,
+				onComplete: function(twn:FlxTween) {
+					alphaTween = null;
+					remove(this);
+					if(onFinish != null) onFinish();
+				}
+			});
+		}});
+	}
+
+	override function destroy() {
+		if(alphaTween != null) {
+			alphaTween.cancel();
+		}
+		super.destroy();
+	}
+}
